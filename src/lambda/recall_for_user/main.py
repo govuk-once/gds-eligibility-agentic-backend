8c3663373917dc1b68b35b4c0bd7b32d4afd8de8
Toby Petty <tobypetty1@gmail.com>
2025-10-27 10:54:43 +0000

    Redeployment after previous aws-nuke run

--- src/lambda/recall_for_user/main.py
+++ src/lambda/recall_for_user/main.py
@@ -1,2 +1,10 @@
-def lambda_handler():
+import logging
+
+import boto3
+
+logger = logging.getLogger()
+
+def lambda_handler(event, context):
+    logger.info(f"event: {event}", extra=dict(event=event))
+    logger.info(f"context: {context}", extra=dict(context=context))
     print("Lambda handled!")
