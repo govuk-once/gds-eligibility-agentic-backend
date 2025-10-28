7d885348867a4bb06e69d41481118a24b00b153f
Toby Petty <tobypetty1@gmail.com>
2025-10-28 11:46:55 +0000

    Pivot back to agent, abstract prompt into variable

--- main.tf
+++ main.tf
@@ -13,6 +13,19 @@ provider "aws" {
 
 locals {
   model_id = "amazon.nova-lite-v1:0"
+  system_instruction = <<-EOT
+    <context>
+    You are tasked with assessing whether the person you are in conversation with is
+    eligable for one or more benefits. You can ask the person as many questions as are needed to
+    assess their eligability, but must only ask one question at a ti:me.
+    Don't provide links
+
+    You must only use the links provided in this context block to understand the criteria for eligability.
+
+    These benefits, are:
+    * Blue Badge: https://www.gov.uk/government/publications/blue-badge-can-i-get-one/can-i-get-a-blue-badge
+    </context>
+  EOT
 }
 
 resource "aws_iam_role" "bedrock_agent_role" {
@@ -77,12 +90,12 @@ resource "aws_iam_role_policy_attachment" "bedrock_agent_policy_limited_access" 
   policy_arn = "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"
 }
 
-# resource "aws_bedrockagent_agent" "eligability_agent" {
-#   agent_name              = "gds_eligability_terraform_sandbox_eligability_agent-${terraform.workspace}"
-#   agent_resource_role_arn = aws_iam_role.bedrock_agent_role.arn
-#   foundation_model        = local.model_id
-#   instruction             = var.agent_instruction
-# }
+resource "aws_bedrockagent_agent" "eligability_agent" {
+  agent_name              = "gds_eligability_terraform_sandbox_eligability_agent-${terraform.workspace}"
+  agent_resource_role_arn = aws_iam_role.bedrock_agent_role.arn
+  foundation_model        = local.model_id
+  instruction             = local.system_instruction
+}
 
 # # This doesn't actually work as descibed by the terraform documentation, and generates errononeous conflicts
 # # See https://github.com/hashicorp/terraform-provider-aws/issues/43045
@@ -93,66 +106,55 @@ resource "aws_iam_role_policy_attachment" "bedrock_agent_policy_limited_access" 
 # #     parent_action_group_signature = "AMAZON.UserInput"
 # }
 
-# resource "aws_bedrockagent_agent_alias" "eligability_alias_for_prompts" {
-#   agent_alias_name = "eligability-alias-for-prompts-${terraform.workspace}"
-#   agent_id         = aws_bedrockagent_agent.eligability_agent.agent_id
-#   description      = "Alias to allow linkage between eligability agent and prompts"
-# }
-
-resource "aws_bedrockagent_prompt" "triage_prompt" {
-  name            = "triage_prompt_${terraform.workspace}"
-  description     = "This is an entrypoint prompt to triage the users initial input"
-  default_variant = "triage_variant"
-
-
-  variant {
-    name          = "triage_variant"
-    model_id      = local.model_id
-    template_type = "CHAT"
-
-    inference_configuration {
-      text {
-        max_tokens     = 2048
-        stop_sequences = ["User:"]
-        temperature    = 0
-        top_p          = 0.8999999761581421
-      }
-    }
-
-    template_configuration {
-      chat {
-        system {
-          text = <<-EOT
-            <context>
-            You are tasked with assessing whether the person you are in conversation with is
-            eligable for one or more benefits. You can ask the person as many questions as are needed to
-            assess their eligability.
-
-            You must only use the links provided in this context block to understand the criteria for eligability.
-
-            These benefits, are:
-            * Blue Badge: https://www.gov.uk/government/publications/blue-badge-can-i-get-one/can-i-get-a-blue-badge
-            </context>
-          EOT
-        }
-        message {
-          role = "user"
-          content {
-            text = "{{user_input}}"
-          }
-        }
-        input_variable {
-          name = "user_input"
-        }
-      }
-    }
-    # gen_ai_resource {
-    #   agent {
-    #     agent_identifier = aws_bedrockagent_agent_alias.eligability_alias_for_prompts.agent_alias_arn
-    #   }
-    # }
-  }
+resource "aws_bedrockagent_agent_alias" "eligability_alias_for_prompts" {
+  agent_alias_name = "eligability-alias-for-prompts-${terraform.workspace}"
+  agent_id         = aws_bedrockagent_agent.eligability_agent.agent_id
+  description      = "Alias to allow linkage between eligability agent and prompts"
 }
+
+# resource "aws_bedrockagent_prompt" "triage_prompt" {
+#   name            = "triage_prompt_${terraform.workspace}"
+#   description     = "This is an entrypoint prompt to triage the users initial input"
+#   default_variant = "triage_variant"
+
+
+#   variant {
+#     name          = "triage_variant"
+#     model_id      = local.model_id
+#     template_type = "CHAT"
+
+#     inference_configuration {
+#       text {
+#         max_tokens     = 2048
+#         stop_sequences = ["User:"]
+#         temperature    = 0
+#         top_p          = 0.8999999761581421
+#       }
+#     }
+
+#     template_configuration {
+#       chat {
+#         system {
+#           text = local.system_instruction
+#         }
+#         message {
+#           role = "user"
+#           content {
+#             text = "{{user_input}}"
+#           }
+#         }
+#         input_variable {
+#           name = "user_input"
+#         }
+#       }
+#     }
+#     # gen_ai_resource {
+#     #   agent {
+#     #     agent_identifier = aws_bedrockagent_agent_alias.eligability_alias_for_prompts.agent_alias_arn
+#     #   }
+#     # }
+#   }
+# }
 
 resource "aws_bedrockagent_flow" "triage" {
   name               = "triage-flow-${terraform.workspace}"
@@ -167,8 +169,8 @@ resource "aws_bedrockagent_flow" "triage" {
       configuration {
         data {
           source_output = "document"
-          # target_input  = "agentInputText"
-          target_input  = "user_input"
+          target_input  = "agentInputText"
+          # target_input  = "user_input"
         }
       }
     }
@@ -180,8 +182,8 @@ resource "aws_bedrockagent_flow" "triage" {
       type   = "Data"
       configuration {
         data {
-          # source_output = "agentResponse"
-          source_output = "modelCompletion"
+          source_output = "agentResponse"
+          # source_output = "modelCompletion"
           target_input  = "document"
         }
       }
@@ -199,56 +201,57 @@ resource "aws_bedrockagent_flow" "triage" {
       }
     }
 
-    node {
-      name = "Triage"
-      type = "Prompt"
-      configuration {
-        prompt {
-          source_configuration {
-            resource {
-              prompt_arn = aws_bedrockagent_prompt.triage_prompt.arn
-            }
-          }
-        }
-      }
-      input {
-        expression = "$.data"
-        name       = "user_input"
-        type       = "String"
-      }
-      output {
-        name       = "modelCompletion"
-        type       = "String"
-      }
-    }
     # node {
-    #   name = "Agent_1"
-    #   type = "Agent"
+    #   name = "Triage"
+    #   type = "Prompt"
     #   configuration {
-    #     agent {
-    #       agent_alias_arn = aws_bedrockagent_agent_alias.eligability_alias_for_prompts.agent_alias_arn
+    #     prompt {
+    #       source_configuration {
+    #         resource {
+    #           prompt_arn = aws_bedrockagent_prompt.triage_prompt.arn
+    #         }
+    #       }
     #     }
     #   }
     #   input {
     #     expression = "$.data"
-    #     name       = "agentInputText"
+    #     name       = "user_input"
     #     type       = "String"
     #   }
-    #   input {
-    #     expression = "$.data"
-    #     name       = "promptAttributes"
-    #     type       = "Object"
-    #   }
-    #   input {
-    #     expression = "$.data"
-    #     name       = "sessionAttributes"
-    #     type       = "Object"
-    #   }
     #   output {
-    #     name = "agentResponse"
-    #     type = "String"
+    #     name       = "modelCompletion"
+    #     type       = "String"
     #   }
     # }
+
+    node {
+      name = "Agent_1"
+      type = "Agent"
+      configuration {
+        agent {
+          agent_alias_arn = aws_bedrockagent_agent_alias.eligability_alias_for_prompts.agent_alias_arn
+        }
+      }
+      input {
+        expression = "$.data"
+        name       = "agentInputText"
+        type       = "String"
+      }
+      input {
+        expression = "$.data"
+        name       = "promptAttributes"
+        type       = "Object"
+      }
+      input {
+        expression = "$.data"
+        name       = "sessionAttributes"
+        type       = "Object"
+      }
+      output {
+        name = "agentResponse"
+        type = "String"
+      }
+    }
 
     node {
       name = "FlowOutputNode"
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "agent_instruction" {
-  description = "Instructions for the agent"
-  type        = string
-  default     = "You are a helpful assistant. Who's a good assistant? You are! Yes, you are, yes you are"
-}
