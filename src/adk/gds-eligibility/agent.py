726f4f172b0ad2b76cd167b7fc67b0cf9358ddea
Toby Petty <tobypetty1@gmail.com>
2025-11-12 14:52:31 +0000

    Use same agent as deployed by terraform

--- src/adk/gds-eligibility/agent.py
+++ src/adk/gds-eligibility/agent.py
@@ -2,7 +2,8 @@ from google.adk.models.lite_llm import LiteLlm
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model=LiteLlm(model="bedrock/converse/amazon.nova-lite-v1:0"),
+    #  model=LiteLlm(model="bedrock/converse/amazon.nova-lite-v1:0"),
+    model = LiteLlm(model="bedrock/agent/CBVQSIPNEW/JU2SFMBBHR"),
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
