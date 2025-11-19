344ee698d4a0fda3fec7fc490ed3e028add653ed
Toby Petty <tobypetty1@gmail.com>
2025-11-19 15:49:52 +0000

    Hacked together a bedrock_agentcore.yaml from an example, as the CLI wouldn't populate it from scratch

--- src/strands/main.py
+++ src/strands/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from strands!")
+from strands import Agent
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
 
+agent = Agent(
+    model="amazon.nova-lite-v1:0"
+)
+app = BedrockAgentCoreApp()
+
+@app.entrypoint
+def invoke(payload):
+    """Process user input and return a response"""
+    user_message = payload.get("prompt", "Hello")
+    response = agent(user_message)
+    return str(response) # response should be json serializable
 
 if __name__ == "__main__":
-    main()
+    app.run(port=8237)
+
