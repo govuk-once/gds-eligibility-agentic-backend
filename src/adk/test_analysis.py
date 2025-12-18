c02080e514a5ed52454392272b6073fb73e7e98e
Toby Petty <tobypetty1@gmail.com>
2025-12-18 18:21:02 +0000

    Make sure analysis results are proportional to number of executions of a permutation

--- src/adk/test_analysis.py
+++ src/adk/test_analysis.py
@@ -1,20 +1,96 @@
+#!/usr/bin/env ipython
 import re
 import pandas as pd
 from pathlib import Path
 import subprocess
 
-output = {}
-output_df = {}
-for output_dir in Path('.testOutputs').glob('*'):
+
+failure_df = {}
+success_df = {}
+combined_df = {}
+
+
+def invert_mapping(dictionary: dict) -> dict:
+    return {elem: k for k, v in dictionary.items() for elem in v}
+
+
+model_size_commit_mapping = {
+    "child_benefit": invert_mapping({
+        "small": [
+            "Unknown",
+            "fc922c5",
+            "9493282",
+            "f2dc127"
+        ],
+        "large": [
+            "575b5e9"
+        ],
+    }),
+    "skilled_worker_visa": invert_mapping({
+        "small": [
+            "Unknown",
+            "db37bc9",
+            "df1795d",
+            "71b1d7c"
+        ],
+        "large": [
+            "8ed3c90"
+        ]
+    })
+}
+
+
+def extract_results_for_folder(output_dir, search_character):
     print(output_dir)
     extracted_records = []
-    output[output_dir] = subprocess.run(["rg", "✗", "-c", "--hidden"], capture_output=True, check=False, text=True, cwd=output_dir)
-    for filename in output[output_dir].stdout.strip().split("\n"):
+    output = subprocess.run(["rg", search_character, "-c", "--hidden"], capture_output=True, check=False, text=True, cwd=output_dir)
+    for filename in output.stdout.strip().split("\n"):
         if filename:
-            if re.match(r"(?P<exec_time>[\d:T\.-]+)__RepoCommit=(?P<commit>[a-f0-9]+)/Permutation(?P<permutation>\d+).*", filename):
-                extracted_fields = re.search(r"(?P<exec_time>[\d:T\.-]+)__RepoCommit=(?P<commit>[a-f0-9]+)/Permutation(?P<permutation>\d+).*", filename).groupdict()
+            if re.match(
+                r"(?P<exec_time>[\d:T\.-]+)__RepoCommit=(?P<commit>[a-f0-9]+)/Permutation(?P<permutation>\d+).*",
+                filename
+            ):
+                extracted_fields = re.search(
+                    r"(?P<exec_time>[\d:T\.-]+)__RepoCommit=(?P<commit>[a-f0-9]+)/Permutation(?P<permutation>\d+).*",
+                    filename
+                ).groupdict()
             else:
-                extracted_fields = re.search(r"(?P<exec_time>[\d:T\.-]+)/Permutation(?P<permutation>\d+).out", filename).groupdict()
+                extracted_fields = re.search(
+                    r"(?P<exec_time>[\d:T\.-]+)/Permutation(?P<permutation>\d+).out",
+                    filename
+                ).groupdict()
             extracted_records.append(extracted_fields)
-    output_df[output_dir] = pd.DataFrame.from_records(extracted_records)
-    print(output_df[output_dir].value_counts("permutation"))
+    return pd.DataFrame.from_records(extracted_records)
+
+
+def main():
+    for output_dir in Path('.testOutputs').glob('*'):
+        test_case= str(output_dir.relative_to(".testOutputs"))
+        print('failures:')
+        failure_df[test_case] = extract_results_for_folder(output_dir, "✗")
+        print(failure_df[test_case].value_counts("permutation"))
+        print('successes:')
+        success_df[test_case] = extract_results_for_folder(output_dir, "✓")
+        print(success_df[test_case].value_counts("permutation"))
+
+        success_df[test_case]["Passed"] = True
+        failure_df[test_case]["Passed"] = False
+        success_df[test_case]["commit"].fillna("Unknown", inplace=True)
+        failure_df[test_case]["commit"].fillna("Unknown", inplace=True)
+
+        success_df[test_case].set_index(["commit", "exec_time", "permutation"], inplace=True)
+        failure_df[test_case].set_index(["commit", "exec_time", "permutation"], inplace=True)
+        combined_df[test_case] = pd.concat(
+            [
+                success_df[test_case],
+                failure_df[test_case]
+            ],
+        )
+        combined_df[test_case]["ModelSize"] = combined_df[test_case].index.get_level_values(0).map(model_size_commit_mapping[test_case])
+        print(combined_df[test_case].value_counts(["Passed", "ModelSize"]))
+        success_rates_by_permutation = combined_df[test_case][combined_df[test_case]["Passed"] == True].index.get_level_values(2).value_counts() / combined_df[test_case].index.get_level_values(2).value_counts()
+        print(success_rates_by_permutation.nsmallest())
+
+
+if __name__ == "__main__":
+    main()
